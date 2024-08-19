99a8c2cd51ba241d9f53ee9887cbe8f73ecaf71e
veraikhalo <ikhalo.oseaghe@gmail.com>
2024-08-19 12:51:45 +0100

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import { ClerkProvider } from "@clerk/nextjs"
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+      <ClerkProvider>
+      <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+        {children}
+      </div>
+      </ClerkProvider>
+    )
+  }
+  
+  export default Layout
